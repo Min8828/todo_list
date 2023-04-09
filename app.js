115c82dffb7f9cc8c32c862e96e6b9c0aa61db84
Min8828 <minrou9928@gmail.com>
2023-04-09 22:33:45 +0800

    fix: fetch correct Todo data in app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ db.once('open', () => console.log('mongodb connected!')) // 連線成功
 
 app.get('/', async (req, res) => {
   try {
-    const todos = Todo.find({}).lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    const todos = await Todo.find({}).lean().exec() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     res.render('index', { todos })
   } catch {
     (err) => console.log(err)
